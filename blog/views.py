dc7993445353043b424f21de4205ea570781e126
setevoybog <15861500+setevoybog@users.noreply.github.com>
2020-11-02 13:11:14 +0300

    add dummy data

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# dummy data
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': '02.11.2020'
+    },
+    {
+        'author': 'Jane Dow',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': '03.11.2020'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
+
+
+
+
+
+
+
+
+
+
