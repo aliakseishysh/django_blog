fe8358efdf0a05443e88c03faf07397e0223b3c1
setevoybog <15861500+setevoybog@users.noreply.github.com>
2020-11-02 15:25:05 +0300

    replace dummy data with database query

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# dummy data
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First Post Content',
-        'date_posted': '02.11.2020'
-    },
-    {
-        'author': 'Jane Dow',
-        'title': 'Blog Post 2',
-        'content': 'Second Post Content',
-        'date_posted': '03.11.2020'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
